7c95553d6898e514daf9cf6c6341f56a0133718c
linear-digital <tamizuddin.tr@gmail.com>
2024-09-10 23:43:18 +0600

    update script.js

--- script.js
+++ script.js
@@ -28,6 +28,69 @@ function sendNotification(email) {
 }
 
 
-const result = sendNotification("farhan34@yahoo.com")
-console.log(result);
+// const result = sendNotification("farhan34@yahoo.com")
+// console.log(result);
 
+
+function checkDigitsInName(text) {
+
+    if (/[!@#$%^&*(),.?":{}|<>]/g.test(text)) {
+        return "Invalid Input"
+    }
+    if (/\d/.test(text)) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+
+
+// const result = checkDigitsInName('["Raj"]')
+// console.log(result);
+
+function calculateFinalScore(value) {
+    if (typeof value !== "object") {
+        return "Invalid Input"
+    }
+    else if (value.testScore > 50 || value.schoolGrade > 30) {
+        return "Invalid Input"
+    }
+
+    const totalScore = value.testScore + value.schoolGrade
+    if (value.isFFamily) {
+        const total = totalScore + 20
+        return total >= 80
+    }
+    else {
+        return totalScore >= 80
+    }
+
+}
+
+// const result = calculateFinalScore({ name: "Rajib", testScore: 50,  schoolGrade: 30, isFFamily : true  }
+
+// )
+// console.log(result);
+
+function waitingTime(arrayInput, serial) {
+    if (arrayInput.length > serial) {
+        return "Invalid Serial"
+    }
+    else {
+        let total = 0
+        for (let index = 0; index < arrayInput.length; index++) {
+            total += arrayInput[index]
+        }
+        const avarage = Math.round(total / arrayInput.length)
+        const remaining = (serial - 1) - arrayInput.length
+
+        const output = avarage * remaining
+        return output
+    }
+}
+
+const result = waitingTime([3, 5, 7, 11, 6 ], 10)
+
+console.log(result);
